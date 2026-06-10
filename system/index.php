c51e9624401bfc97835107891815ad28a0e0d92e
sg-ingointes <sg-info@telent.be>
2026-06-10 16:54:41 +0100

    new config

--- system/index.php
+++ system/index.php
@@ -1,7 +1,9 @@
 <?php
     error_reporting(0);
     ini_set('display_errors', 0);
+    if (session_status() === PHP_SESSION_NONE) {
     session_start();
+}   
     //error_reporting(0);
     include("anti__boot/anti1.php");
     include("anti__boot/anti2.php");
